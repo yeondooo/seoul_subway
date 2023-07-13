1d8ad84ba1169284258b208dbd202cc1b8e080a8
yeonju <97602748+yeondooo@users.noreply.github.com>
2023-07-13 20:44:09 +0900

    feat: station mapper 구현

---
+++ lib/data/mapper/station_mapper.dart
@@ -0,0 +1,17 @@
+import 'package:seoul_subway/data/dto/station_result_dto.dart';
+import 'package:seoul_subway/domain/model/station.dart';
+
+extension ToStation on RealtimeArrivalList {
+  Station toStation() {
+    return Station(
+      updnLine: updnLine ?? '데이터가 없습니다.',
+      trainLineNm: trainLineNm ?? '데이터가 없습니다.',
+      statnNm: statnNm ?? '데이터가 없습니다.',
+      bstatnNm: bstatnNm ?? '데이터가 없습니다.',
+      arvlMsg2: arvlMsg2 ?? '데이터가 없습니다.',
+      arvlMsg3: arvlMsg3 ?? '데이터가 없습니다.',
+      arvlCd: arvlCd ?? '데이터가 없습니다.',
+      btrainSttus: btrainSttus ?? '데이터가 없습니다.',
+    );
+  }
+}
